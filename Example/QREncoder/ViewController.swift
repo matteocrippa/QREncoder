c36cf4f50b836c6dac4525249f68b5dd054cf766
Matteo <matteo@boostco.de>
2016-08-08 09:03:33 +0200

    Initial commit

---
+++ Example/QREncoder/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  QREncoder
+//
+//  Created by Matteo on 08/08/2016.
+//  Copyright (c) 2016 Matteo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
